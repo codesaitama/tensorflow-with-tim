37dbe996887aed385ef06193e1ceefc5fe61d30d
Emmanuel Achana <achanamosey@gmail.com>
2020-09-21 16:44:08 +0000

    Added prediction.

--- codes/tutorial-one.py
+++ codes/tutorial-one.py
@@ -21,8 +21,12 @@ model = keras.Sequential([
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(train_images, train_labels, epochs=5)
+model.fit(train_images, train_labels, epochs=2)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+prediction = model.predict(test_images)
 
-print("Tested Acc: ", test_acc)
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("prediction" + class_names[np.argmax(prediction[i])])
