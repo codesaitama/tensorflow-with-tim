e186d8c0cf0c0f59d734427616c7f3df438b8a26
Emmanuel Achana <achanamosey@gmail.com>
2020-09-27 08:38:19 +0000

    Fix a bug in the iteration.

--- test-scripts/best-racer.py
+++ test-scripts/best-racer.py
@@ -8,23 +8,37 @@ def best_items(racers):
         racer = racers[i]
         # We're only interested in racers who finished in first
         if racer['finish'] == 1:
-            for i in racer['items']:
+            for x in racer['items']:
                 # Add one to the count for this item (adding it to the dict if necessary)
-                if i not in winner_item_counts:
+                if x not in winner_item_counts:
                     winner_item_counts[i] = 0
                 winner_item_counts[i] += 1
 
         # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
         if racer['name'] is None:
-            print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format( i+1, len(racers), racer['name']) )
+            print(f"WARNING: Encountered racer with unknown name on iteration {i + 1}/{len(racers)} (racer = {racer['name']})")
     return winner_item_counts
 
 
-sample = [
+sample_one = [
     {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
     {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
     {'name': None, 'items': ['mushroom',], 'finish': 2},
     {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
 ]
 
-print(best_items(sample))
+sample_two = [
+{'name': 'Peach', 'items': ['green shell', 'banana', 'green shell'], 'finish': 3}, 
+{'name': 'Peach', 'items': ['green shell', 'banana', 'green shell'], 'finish': 1}, {'name': 'Bowser', 'items': ['green shell'], 'finish': 1}, 
+{'name': None, 'items': ['green shell'], 'finish': 2}, 
+{'name': 'Bowser', 'items': ['green shell'], 'finish': 1}, 
+{'name': None, 'items': ['red shell'], 'finish': 1}, 
+{'name': 'Yoshi', 'items': ['banana', 'blue shell', 'banana'], 'finish': 7}, 
+{'name': 'DK', 'items': ['blue shell', 'star'], 'finish': 1}
+]
+
+print(best_items(sample_one))
+print('-----' * 20)
+print(best_items(sample_two))
+
+# print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format( i + 1, len(racers), racer['name']) )
