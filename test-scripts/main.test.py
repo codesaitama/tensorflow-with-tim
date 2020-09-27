fa0a725976e1e9cc8999c8096670c2133c836443
Emmanuel Achana <achanamosey@gmail.com>
2020-09-27 08:07:02 +0000

    Going on

--- test-scripts/main.test.py
+++ test-scripts/main.test.py
@@ -1,4 +1,6 @@
 import numpy as np
+# import tensorflow as tf
+import matplotlib as mtp
 
 rolls = np.random.randint(low=1, high=6, size=10)
 
@@ -10,7 +12,12 @@ xlist = [[1,2,3],[2,4,6],]
 # Create a 2-dimensional array
 x = np.asarray(xlist)
 
-print("xlist = {}\nx = {}\n".format(xlist, x))
-print(f'xlist = {xlist}\nx = {x}')
+# print("xlist = {}\nx = {}\n".format(xlist, x))
+# print(f'xlist = {xlist}\nx = {x}')
 
-print(xlist[1][-1]) # Get the last item from the second array in the two dimensional array.
+# print(xlist[1][-1]) # Get the last item from the second array in the two dimensional array.
+
+lst = [0, 100, 200, 300, 400, 500]
+
+ans = ['${}'.format(item) for item in lst]
+print(ans)
