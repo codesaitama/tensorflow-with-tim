23f99b8624c14529907b5bee788ac8f9d15a9304
Emmanuel Achana <achanamosey@gmail.com>
2020-09-27 06:32:12 +0000

    Two dimensional array with numpy

--- test-scripts/main.test.py
+++ test-scripts/main.test.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 rolls = np.random.randint(low=1, high=6, size=10)
-arr = np.array([1, 2, 3, 4, 5])
 
 print(rolls)
 print(rolls <= 3)
-
 print(rolls + 10)
+
+xlist = [[1,2,3],[2,4,6],]
+# Create a 2-dimensional array
+x = np.asarray(xlist)
+print("xlist = {}\nx = {}\n".format(xlist, x))
