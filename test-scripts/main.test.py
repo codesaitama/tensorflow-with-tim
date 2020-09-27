7d2a096adf92f05a63c5d700e518309169d3348a
Emmanuel Achana <achanamosey@gmail.com>
2020-09-27 06:39:07 +0000

    Using index to get elements. Python

--- test-scripts/main.test.py
+++ test-scripts/main.test.py
@@ -2,9 +2,9 @@ import numpy as np
 
 rolls = np.random.randint(low=1, high=6, size=10)
 
-print(rolls)
-print(rolls <= 3)
-print(rolls + 10)
+# print(rolls)
+# print(rolls <= 3)
+# print(rolls + 10)
 
 xlist = [[1,2,3],[2,4,6],]
 # Create a 2-dimensional array
@@ -12,3 +12,5 @@ x = np.asarray(xlist)
 
 print("xlist = {}\nx = {}\n".format(xlist, x))
 print(f'xlist = {xlist}\nx = {x}')
+
+print(xlist[1][-1]) # Get the last item from the second array in the two dimensional array.
